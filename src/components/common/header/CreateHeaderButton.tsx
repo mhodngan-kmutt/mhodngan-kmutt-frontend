eb5c5c6ea305b7eaa876b45e2183e492294f5000
2005fpp <pheen.phoorin@gmail.com>
2025-10-30 06:21:47 +0700

    feat: write-project-button when signed in

---
+++ src/components/common/header/CreateHeaderButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import WriteIcon from '../../../assets/icons/writeIcon.tsx';
+
+interface CreateHeaderButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  lang: string;
+}
+
+export default function CreateHeaderButton({
+  lang,
+}: CreateHeaderButtonProps) {
+  return (
+    <a href={lang === 'th' ? '/th/create' : '/en/create'}>
+      <button
+        type="button"
+        aria-label="Write Project"
+        className="btn-primary flex items-center gap-2"
+      >
+        <WriteIcon className="w-5 h-5 " />
+        <span className="small">Write Project</span>
+      </button>
+    </a>
+  );
+}
