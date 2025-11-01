9abd647fbc0c72c7ebcbf51c6623d49d6ad57b7e
2005fpp <pheen.phoorin@gmail.com>
2025-11-02 00:44:12 +0700

    chore: fix margin searchbar

--- src/components/ui/searchbar.tsx
+++ src/components/ui/searchbar.tsx
@@ -25,7 +25,7 @@ export function Searchbar({
       {/* Clickable Search icon */}
       <button
         type="submit"
-        className="absolute inset-y-0 left-0 flex items-center justify-center ml-1 my-2.5 text-muted-foreground hover:text-neutral-800"
+        className="absolute inset-y-0 left-0 flex items-center justify-center my-2.5 text-muted-foreground hover:text-neutral-800"
       >
         <Search className="w-5 h-5 text-neutral-500 hover:text-neutral-800" />
       </button>
